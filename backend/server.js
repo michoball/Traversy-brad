c6c80e7351e32c76ddf701adcdaf80e39ef48057
michoball <myunghun0114@gmail.com>
2022-03-30 00:29:22 +0900

    fix deployment

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,12 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 //Serve Frontend
-
 if (process.env.NODE_ENV === "production") {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  app.get("*", (_, res) =>
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
