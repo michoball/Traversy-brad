c315881f039809091dd11c742d7f61bd69c45242
michoball <myunghun0114@gmail.com>
2022-04-02 18:34:08 +0900

    fix mistake

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ const { underline } = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://kang1234:rkdaudgns@traversycluster.2fiy9.mongodb.net/supportdeskdb?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connnected : ${conn.connection.host} `.cyan.underline);
   } catch (error) {
     console.log(`Error : ${error.message}`.red.underline.bold);
